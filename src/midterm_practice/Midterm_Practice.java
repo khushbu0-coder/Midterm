0fcc899df2ff747f63ac4f18dfe7c99d82d1a306
Khushbu Khushbu <khushbu0@sheridancollege.ca>
2024-10-14 17:18:40 -0400

    Welcome message and JASOOSI

---
+++ src/midterm_practice/Midterm_Practice.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package midterm_practice;
+
+/**
+ *
+ * @author khush
+ */
+public class Midterm_Practice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("Hello World!");
+        System.out.println("20 Bear Run Road: DCRY 721");
+    }
+    
+}
